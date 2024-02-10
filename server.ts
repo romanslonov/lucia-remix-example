d9d2959237358603f9b2a1c2165c36c467e12980
Roman Slonov <destabiliseus@gmail.com>
2024-02-11 02:53:55 +0800

    refactor: rename middleware, add csrf protection

--- server.ts
+++ server.ts
@@ -2,13 +2,33 @@ import { createRequestHandler } from "@remix-run/express";
 import { broadcastDevReady, installGlobals } from "@remix-run/node";
 import express from "express";
 import * as build from "@remix-run/dev/server-build";
-import { auth } from "middleware";
+import { auth } from "./auth";
+import { verifyRequestOrigin } from "lucia";
 
 // patch in Remix runtime globals
 installGlobals();
 
 const app = express();
 app.use(express.static("public"));
+
+// CSRF protection
+app.use((request, response, next) => {
+  if (request.method === "GET") {
+    return next();
+  }
+  const originHeader = request.headers.origin ?? null;
+  // NOTE: You may need to use `X-Forwarded-Host` instead
+  const hostHeader = request.headers.host ?? null;
+  if (
+    !originHeader ||
+    !hostHeader ||
+    !verifyRequestOrigin(originHeader, [hostHeader])
+  ) {
+    return response.status(403).end();
+  }
+
+  return next();
+});
 
 // and your app is "just a request handler"
 app.all(
